f72a2f6b97e459af93e624d93ad537c3db0eeb48
delta4d <delta4d@gmail.com>
2014-09-17 22:49:25 +0800

    add opposite bfs

--- leetcode/surrounded-regions.cpp
+++ leetcode/surrounded-regions.cpp
@@ -44,3 +44,42 @@ public:
         }
     }
 };
+
+// 88ms
+// another bfs
+int const N = 1 << 10;
+int const dx[] = {0, 0, 1, -1};
+int const dy[] = {1, -1, 0, 0};
+bool v[N][N];
+class Solution {
+public:
+    void bfs(int x, int y, int m, int n, vector<vector<char>> &board) {
+        queue <pair<int,int>> q;
+        q.push({x, y});
+        v[x][y] = true;
+        while (!q.empty()) {
+            x = q.front().first, y = q.front().second;
+            q.pop();
+            for (int k=0; k<4; ++k) {
+                int tx = x + dx[k], ty = y + dy[k];
+                if (tx < 0 || tx >= m || ty < 0 || ty >= n || v[tx][ty] || board[tx][ty] == 'X') continue;
+                v[tx][ty] = true;
+                q.push({tx, ty});
+            }
+        }
+    }
+    void solve(vector<vector<char>> &board) {
+        if (board.empty()) return;
+        memset(v, false, sizeof(v));
+        int m = (int)board.size(), n = (int)board.front().size();
+        for (int i=0; i<n; ++i) {
+            if (board[0][i] == 'O' && !v[0][i]) bfs(0, i, m, n, board);
+            if (board[m-1][i] == 'O' && !v[m-1][i]) bfs(m-1, i, m, n, board);
+        }
+        for (int i=0; i<m; ++i) {
+            if (board[i][0] == 'O' && !v[i][0]) bfs(i, 0, m, n, board);
+            if (board[i][n-1] == 'O' && !v[i][n-1]) bfs(i, n-1, m, n, board);
+        }
+        for (int i=0; i<m; ++i) for (int j=0; j<n; ++j) if (!v[i][j]) board[i][j] = 'X';
+    }
+};
