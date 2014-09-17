7fa38793c855307a6abe13330f780fdcdf427564
delta4d <delta4d@gmail.com>
2014-09-17 20:54:03 +0800

    add bitmask solution

--- leetcode/single-number-ii.cpp
+++ leetcode/single-number-ii.cpp
@@ -25,3 +25,28 @@ public:
         return ret;
     }
 };
+
+// 88ms
+// bit mask
+using ull = unsigned long long;
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        ull state = 0;
+        for (int i=0; i<n; ++i) {
+            for (int x=A[i]; x; x&=x-1) {
+				ull mask = x - (x & (x - 1));
+				mask *= mask;
+				ull bit_state = (state / mask) & 3;
+				bit_state = (bit_state + 1) % 3;
+				state &= ~(mask | (mask << 1));
+				state ^= bit_state * mask;
+            }
+        }
+        int ret = 0;
+        for (ull i=0; i<32; ++i) {
+            ret ^= ((state >> (i << 1)) & 1) << i;
+        }
+        return ret;
+    }
+};
