ee0780f75734dd96a702b389211a11eac5cfd622
delta4d <delta4d@gmail.com>
2014-09-17 14:18:57 +0800

    add O(1) space solution

--- leetcode/linked-list-cycle-ii.cpp
+++ leetcode/linked-list-cycle-ii.cpp
@@ -19,3 +19,23 @@ public:
         return detectCycle(head->next);
     }
 };
+
+// O(1) space
+// line length a, cycle length c, meet pos in cycle x, meet time t
+// t  - a = x (mod c)
+// 2t - a = x (mod c)
+// => a + x = t = 0 (mod c)
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *a = head, *b = head;
+        while (a != nullptr && b != nullptr && b->next != nullptr) {
+            a = a->next, b = b->next->next;
+            if (a == b) break;
+        }
+        if (a == nullptr || b == nullptr || b->next == nullptr) return nullptr;
+        a = head;
+        while (a != b) a = a->next, b = b->next;
+        return a;
+    }
+};
