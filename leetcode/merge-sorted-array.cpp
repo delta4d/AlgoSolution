3cd369ca17c3444791db5e06ef70afcc41217fa6
delta4d <delta4d@gmail.com>
2014-09-18 21:00:11 +0800

    add a slightly neat one

--- leetcode/merge-sorted-array.cpp
+++ leetcode/merge-sorted-array.cpp
@@ -13,3 +13,15 @@ public:
         for (; pb<n; A[p++]=B[pb++]);
     }
 };
+
+// merge backwards to avoid copy A[.] to A[.+n]
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        for (int k=m+n-1,i=m-1,j=n-1; j>=0; ) {
+            if (i == -1) A[k--] = B[j--];
+            else if (A[i] < B[j]) A[k--] = B[j--];
+            else A[k--] = A[i--];
+        }
+    }
+};
