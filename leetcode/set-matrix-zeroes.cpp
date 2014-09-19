c54af206dccf4599f8e1e8480fef54e77d82408f
delta4d <delta4d@gmail.com>
2014-09-19 22:27:29 +0800

    add inplace sol

--- leetcode/set-matrix-zeroes.cpp
+++ leetcode/set-matrix-zeroes.cpp
@@ -17,3 +17,26 @@ public:
         }
     }
 };
+
+// inplace
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        if (matrix.empty()) return;
+        int m = (int)matrix.size(), n = (int)matrix.back().size();
+        bool zero_row{}, zero_col{};
+        for (int i=0; i<n; ++i) zero_row = zero_row || matrix[0][i] == 0;
+        for (int j=0; j<m; ++j) zero_col = zero_col || matrix[j][0] == 0;
+        for (int i=1; i<m; ++i) for (int j=1; j<n; ++j) if (matrix[i][j] == 0) {
+            matrix[0][j] = 0, matrix[i][0] = 0;
+        }
+        for (int i=1; i<n; ++i) if (matrix[0][i] == 0) {
+            for (int j=1; j<m; ++j) matrix[j][i] = 0;
+        }
+        for (int i=1; i<m; ++i) if (matrix[i][0] == 0) {
+            for (int j=1; j<n; ++j) matrix[i][j] = 0;
+        }
+        if (zero_row) for (int i=0; i<n; ++i) matrix[0][i] = 0;
+        if (zero_col) for (int i=0; i<m; ++i) matrix[i][0] = 0;
+    }
+};
